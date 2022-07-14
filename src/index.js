b8d1b665249491f9a578109470c7464794c8b971
mark savka <mark.savka.it@gmail.com>
2022-07-14 13:42:06 +0300

    add notifix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+/*
+  * Notiflix.Notify.success('Sol lucet omnibus')
+  ! Notiflix.Notify.failure('Qui timide rogat docet negare')
+  ? Notiflix.Notify.warning('Memento te hominem esse')
+*/
 
 const DEBOUNCE_DELAY = 300;
 const input = document.getElementById('search-box');
@@ -12,11 +19,21 @@ input.addEventListener(
       `https://restcountries.com/v3.1/name/${e.target.value}?fields=name,capital,population,flags,languages`
     )
       .then(res => res.data)
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
+      .then(checkData)
+      .catch(() => Notiflix.Notify.failure('Country not found!!!'));
   }, DEBOUNCE_DELAY)
 );
 
-function checkData (data) {
-  // if()
+function checkData(data) {
+  if (data.length > 10) {
+    Notiflix.Notify.warning(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length === 1) {
+    renderCountry();
+  } else renderCountries();
 }
+
+function renderCountry() {}
+
+function renderCountries() {}
