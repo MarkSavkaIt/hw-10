02e62f00bc3d46a6893e58360dab50c7a9c3e8f7
mark savka <mark.savka.it@gmail.com>
2022-07-14 14:23:54 +0300

    add fetch several countries

--- src/index.js
+++ src/index.js
@@ -6,12 +6,39 @@ import Notiflix from 'notiflix';
 /*
   * Notiflix.Notify.success('Sol lucet omnibus')
   ! Notiflix.Notify.failure('Qui timide rogat docet negare')
-  ? Notiflix.Notify.warning('Memento te hominem esse')
+  ? Notiflix.Notify.info('Memento te hominem esse')
 */
 
 const DEBOUNCE_DELAY = 300;
 const input = document.getElementById('search-box');
-console.log(input);
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+function checkData(data) {
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    countryList.innerHTML = '';
+  } else if (data.length === 1) {
+    renderCountry(data);
+  } else renderCountries(data);
+}
+
+function renderCountry(data) {
+  let html = '';
+}
+
+function renderCountries(data) {
+  let html = data
+    .map(
+      item =>
+        `<li> <img src="${item.flags.svg}" width="30px" /> <p>${item.name.common}</p></li>`
+    )
+    .join('');
+  countryList.innerHTML = html;
+}
+
 input.addEventListener(
   'input',
   debounce(e => {
@@ -19,21 +46,12 @@ input.addEventListener(
       `https://restcountries.com/v3.1/name/${e.target.value}?fields=name,capital,population,flags,languages`
     )
       .then(res => res.data)
-      .then(checkData)
-      .catch(() => Notiflix.Notify.failure('Country not found!!!'));
+      .then(data => checkData(data))
+      .catch(err => {
+        Notiflix.Notify.failure(err.message);
+        countryList.innerHTML = '';
+      });
+
+    // .catch(err => Notiflix.Notify.failure('Country not found!!!'));
   }, DEBOUNCE_DELAY)
 );
-
-function checkData(data) {
-  if (data.length > 10) {
-    Notiflix.Notify.warning(
-      'Too many matches found. Please enter a more specific name.'
-    );
-  } else if (data.length === 1) {
-    renderCountry();
-  } else renderCountries();
-}
-
-function renderCountry() {}
-
-function renderCountries() {}
